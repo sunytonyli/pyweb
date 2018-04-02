978ad662abd131c5b84490d1e5898fc39a7ec002
lisy <sunytonyli@gmail.com>
2018-04-02 19:13:35 +0800

    add ckEditor

--- pyweb/views.py
+++ pyweb/views.py
@@ -4,3 +4,15 @@ from flask import Flask, url_for, render_template
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/demo')
+def demo():
+	return render_template('kartik/examples/index.html')
+
+@app.route('/ckeditor')
+def ckeditor():
+	return render_template('ckeditor.html');
+
+@app.route('/ckeditor/examples')
+def ckeditor_demo():
+	return render_template('ckeditor/samples/index.html');
